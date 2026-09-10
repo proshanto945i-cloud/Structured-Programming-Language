9537e834501cac3738a50742552cf5f72f719461
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-10 23:07:43 +0600

    Add program to print float, double, and char values
    Initial implementation of a program that prints a float, double, and char.

---
+++ project2.c
@@ -0,0 +1,12 @@
+//write a program that prints an integar number
+#include<stdio.h>
+int main(){
+    float num1=10.5;
+    double num2=10.24536232;
+    char ch='a';
+    printf("num1=%.1f\n",num1);
+     printf("num2=%lf\n",num2);
+      printf("ch=%c",ch);
+
+
+}
