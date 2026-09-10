ac11085aeca764b5b9c917d42739399a9859b902
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-10 23:06:19 +0600

    Update project1.c

---
+++ project1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    //variable declartion % initialization
+    int num1=10,num=15;
+    float num3=3.14;
+    double num4=10.13950292;
+    char ch='a';
+printf("int\nfloat\nchar\ndouble");
+
+
+}
