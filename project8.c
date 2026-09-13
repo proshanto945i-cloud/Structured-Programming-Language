bdf59a3d5cc31103948f6aab32aec21d301957c7
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 00:45:45 +0600

    Add main function to calculate sum of three integers
    Initialize three integer variables and print their sum.

---
+++ project8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int a , b ,c ;
+
+    a=b=c=500; // multiple variables declared and initialized in a single line
+
+    printf("%d", a+b+c); // this will print the sum of a, b and c
+    return 0;
+}
