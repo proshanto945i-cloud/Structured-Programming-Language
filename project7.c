d621469af92374035d48565c665f9d39b74a1fec
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 00:39:32 +0600

    Add project7.c to calculate the sum of three numbers

---
+++ project7.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+int main()
+{
+    int a = 5, b = 10, c = 15; // multiple variables declared and initialized in a single line
+    int sum = a + b + c;
+    printf("Sum is: %d\n", sum);
+    return 0;
+}
