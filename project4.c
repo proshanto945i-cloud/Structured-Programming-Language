80c32c12a460f3abaa8331a567a281227fbb6a91
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 00:32:21 +0600

    Add example of variable value change in C

---
+++ project4.c
@@ -0,0 +1,15 @@
+//EXAMPLE 2 of changing values of variables
+
+#include <stdio.h>
+int main()
+ {
+
+    int myNum_1= 15;   // myNum is 15
+
+    int myNum_2 = 20; // myNum_2 is 20
+
+    myNum_1 = myNum_2; // this will update the value of myNum_1 to 20
+
+
+    return 0;
+}
