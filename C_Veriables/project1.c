c9f98952f8abf9f6c449d7baa9bf276392fe266f
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 20:50:32 +0600

    Add example of changing variable values in C

---
+++ C_Veriables/project1.c
@@ -0,0 +1,16 @@
+// EXAMPLE 1 of changing values of variables
+
+#include <stdio.h>
+int main() 
+{
+
+    int myNum = 15;   // myNum is 15
+
+    printf("Integer value is: %d\n", myNum);
+
+    myNum = 20; // this will update the value of myNum to 20
+
+    printf("Updated Integer value is: %d\n", myNum);
+
+    return 0;
+}
