c3665096cb1fd61391ad7b6b275a22fc9f43075f
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 23:12:53 +0600

    Add example program for boolean comparison in C

---
+++ C_Booleans.c/project37.c
@@ -0,0 +1,10 @@
+// EXAMPLE 3 
+
+#include<stdio.h>
+
+int main() 
+{
+    printf("%d",5>2); // prints 1 because 5 is greater than 2
+    return 0;
+    
+}                                           
