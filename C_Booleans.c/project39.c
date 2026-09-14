9a7ed955f60a4a48acf339e886ca050de5a4cc9e
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 23:13:49 +0600

    Add C program to compare boolean values

---
+++ C_Booleans.c/project39.c
@@ -0,0 +1,12 @@
+// EXAMPLE 5 
+
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool isHamburgerTasty = true; // boolean variable with value true
+    bool isPizzaTasty = true; // boolean variable with value true
+
+    printf("%d\n", isHamburgerTasty == isPizzaTasty); // prints 1 because both are true
+    return 0;
+}
