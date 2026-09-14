16189d00572867eca7bf652c309e71ef5ef784ae
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 23:12:26 +0600

    Add example of boolean usage in C

---
+++ C_Booleans.c/project36.c
@@ -0,0 +1,18 @@
+// EXAMPLE 2 
+
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    // create boolean variables 
+
+    bool isProgrammingFun = true; // boolean variable with value true
+    bool isFishTasty = false; // boolean variable with value false
+
+    // print the boolean values
+
+    printf("%d\n", isProgrammingFun); // prints 1 
+    printf("%d", isFishTasty); // prints 0
+    return 0;
+    
+}
