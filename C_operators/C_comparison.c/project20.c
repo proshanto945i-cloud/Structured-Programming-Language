8c1c4c96548c9e902d7b51f5381fa6952d70ee3c
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:40:59 +0600

    Add example for comparison operators in C
    Demonstrates all comparison operators in C with example output.

---
+++ C_operators/C_comparison.c/project20.c
@@ -0,0 +1,17 @@
+// EXAMPLE 1 : all comparison operators in C 
+
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    int y=5;
+
+    printf("%d\n",x==y); // equal to operator 0
+    printf("%d\n",x!=y); // not equal to operator 1
+    printf("%d\n",x>y); // greater than operator 1
+    printf("%d\n",x<y); // less than operator 0
+    printf("%d\n",x>=y); // greater than or equal to operator 1
+    printf("%d\n",x<=y); // less than or equal to operator 0
+
+    return 0;
+}
