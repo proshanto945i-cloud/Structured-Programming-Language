be1187de96e9ba8ea76c78808aa1ad78ae5ba3a4
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:55:06 +0600

    Add student information display program

---
+++ C_Datatype.c/project31.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+    int studentID = 12345;
+    float score = 95.5;
+    char grade = 'A';
+
+    printf("StudentID: %d\nScore: %.2f\nGrade: %c\n", studentID, score, grade);
+    return 0;
+}
