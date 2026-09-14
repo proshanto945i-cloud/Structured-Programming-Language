0d49b608bfc2fe11f866c1b452106b0ef89ce6b8
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:40:02 +0600

    Add example of assignment operators in C
    Added a real-life example demonstrating the use of assignment operators in C.

---
+++ C_operators/C_assignment.c/project19.c
@@ -0,0 +1,13 @@
+// EXAMPLE :  real life example of assignment operators in C
+
+#include<stdio.h> 
+int main()
+{
+    int savings = 1000; // assignment operator
+
+    savings += 500; //add 500 to savings
+    
+    printf("The total savings is: %d\n", savings); // prints 1500
+    return 0;
+    
+}
