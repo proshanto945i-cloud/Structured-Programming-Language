6ce08ef083e6906da0a23c58b9fb93d241d94729
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:33:42 +0600

    move project to C_outputs

--- C_outputs/project13.c
+++ C_outputs/project13.c
@@ -1,1 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello world!\ni am learning C programming language\nand i am enjoying it\n"); //this will print hello world in the output and also print new lines 
 
+     printf("hello world\n\nhave a good day");  //this will skip two lines and print have a good day in the output
+
+    return 0;
+}
--- project13.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("Hello world!\ni am learning C programming language\nand i am enjoying it\n"); //this will print hello world in the output and also print new lines 
-
-     printf("hello world\n\nhave a good day");  //this will skip two lines and print have a good day in the output
-
-    return 0;
-}
