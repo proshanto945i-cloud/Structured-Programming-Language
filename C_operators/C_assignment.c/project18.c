4bcbc0c609eb85dce8da325c876a2a51f3ed581e
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:39:18 +0600

    Add example of all assignment operators in C
    Demonstrates various assignment operators in C.

---
+++ C_operators/C_assignment.c/project18.c
@@ -0,0 +1,20 @@
+// EXAMPLE : all assignment operators in C
+
+#include<stdio.h>
+int main() 
+{
+    int x=5; // assignment operator
+
+    x+=5; // addition assignment operator
+    x-=5; // subtraction assignment operator
+    x*=5; // multiplication assignment operator
+    x/=5; // division assignment operator
+    x%=5; // modulo assignment operator
+    x&=5; // bitwise AND assignment operator 
+    x|=5; // bitwise OR assignment operator
+    x^=5; // bitwise XOR assignment operator
+    x<<=5; // left shift assignment operator
+    x>>=5; // right shift assignment operator
+
+    return 0;
+}
