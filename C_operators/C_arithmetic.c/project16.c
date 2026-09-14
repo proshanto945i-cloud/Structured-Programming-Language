6bd5e49ee5999dfa3c09f813d1badc3d855e1c9c
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:37:17 +0600

    Add example for increment and decrement operators
    Added example demonstrating increment and decrement operators in C.

---
+++ C_operators/C_arithmetic.c/project16.c
@@ -0,0 +1,18 @@
+// EXAMPLE 2 :increment and decrement operators in C
+
+#include<stdio.h>
+int main()
+{
+   int x=10;
+
+   ++x; // increment operator 11 ; this will 1st increment the value of x and then return the value of x
+    printf("%d\n",x);
+
+   int y=10;
+   
+   --y; // decrement operator 9 ; this will 1st decrement the value of y and then return the value of y
+    printf("%d\n",y);
+
+
+    return 0;
+}
