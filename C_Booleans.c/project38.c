1ada47f8d8bbd8e838f47369dd7776658cc32077
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 23:13:20 +0600

    Add example of boolean comparisons in C

---
+++ C_Booleans.c/project38.c
@@ -0,0 +1,14 @@
+// EXAMPLE 4 
+
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    printf("%d",10==10); // prints 1 because 10 is equal to 10
+
+    printf("%d",10==15); // prints 0 because 10 is not equal to 15
+
+    printf("%d",10!=15); // prints 1 because 10 is not equal to 15
+    
+    return 0;
+}
