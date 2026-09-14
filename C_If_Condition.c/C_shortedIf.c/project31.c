fe90d21454ebd1d8e97c0729f1bb639625c68f09
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:53:48 +0600

    Add conditional greeting based on time
    Implement a short if condition to greet based on time.

---
+++ C_If_Condition.c/C_shortedIf.c/project31.c
@@ -0,0 +1,14 @@
+// EXAMPLE 2 ; My own made problem and solution 
+
+#include<stdio.h> 
+#include<stdbool.h>
+int main()
+{
+    int time = 20;
+    bool isTime = time < 18;
+
+    (isTime) ? 
+    printf("good morning") : printf("good evening");
+
+    return 0 ;
+}
