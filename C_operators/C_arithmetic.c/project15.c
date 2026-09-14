60e3f03ae5b6483aeee04937cc6b3754a4c23e79
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:36:26 +0600

    Fix arithmetic operations and formatting in project15.c

--- C_operators/C_arithmetic.c/project15.c
+++ C_operators/C_arithmetic.c/project15.c
@@ -1,1 +1,21 @@
+// EXAMPLE 1 : All arithmetic operators in C
 
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    int y = 3; 
+
+    printf("%d\n",x+y); // addition operator 13
+    printf("%d\n",x-y); // subtraction operator 7
+    printf("%d\n",x*y); // multiplication operator 30
+    printf("%d\n",x/y); // division operator 3
+    printf("%d\n",x%y); // modulo operator 1
+
+    int z=6;
+    ++z; // increment operator 7
+    printf("%d\n",z);
+    --z; // decrement operator 6
+    printf("%d\n",z);
+    return 0;
+}
