e0d0503e58c6ea35d380a2cdaef0f11c5b11f575
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 21:07:16 +0600

    move project to C_typeConversions

--- C_typeConversions/project9.c
+++ C_typeConversions/project9.c
@@ -1,1 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float myFloat = 10;
 
+    printf("The value of myFloat is %f\n",myFloat); // conversion from int to float
+    return 0;
+    
+}
--- project9.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float myFloat = 10;
-
-    printf("The value of myFloat is %f\n",myFloat); // conversion from int to float
-    return 0;
-    
-}
