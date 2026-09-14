a6e2eeb4da232d31df782cfee15ca60a3eb5882b
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:57:13 +0600

    Add example C program with comments

---
+++ C_comment.c/project32.c
@@ -0,0 +1,13 @@
+// EXAMPLE 1 
+
+#include<stdio.h>
+int main()
+{
+    // this is a single line comment
+
+    /* this is a multi line comment
+    this is the second line of the multi line comment */ 
+
+    printf ("Hello World\n"); // prints Hello World
+    return 0;
+}
