498699674ab2b5f64084e750781b127f8e64c2e9
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 23:11:50 +0600

    Add example of boolean usage in C

---
+++ C_Booleans.c/project35.c
@@ -0,0 +1,15 @@
+// EXAMPLE 1 
+
+/*booleans have one of two values like :  
+    true/yes/on = 1
+    false = 0
+*/
+
+#include<stdio.h>
+#include<stdbool.h>
+int main() 
+{
+    bool myBool = true;
+    printf("Boolean value: %d\n", myBool);
+    return 0;
+}
