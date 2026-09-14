6ab8362c749bd9785d29b90d9a4d1c25fe6610e5
PROSHANTO TALUKDER RAJ <proshanto.talukder2008@gmail.com>
2026-09-14 22:41:23 +0600

    Add comparison operators example in project21.c
    This program demonstrates the use of comparison operators in C by evaluating the variable 'age' against different values.

---
+++ C_operators/C_comparison.c/project21.c
@@ -0,0 +1,14 @@
+// EXAMPLE 2 
+
+#include<stdio.h>
+int main()
+{
+    int age = 20;
+
+    printf("%d",age>18); // prints 1 because 20 is greater than 18
+    printf("%d",age<18); // prints 0 because 20 is not less than 18
+    printf("%d",age>=18); // prints 1 because 20 is greater than or equal to 18
+    printf("%d",age<=18); // prints 0 because 20 is not less than or equal to 18
+
+    return 0;
+}
